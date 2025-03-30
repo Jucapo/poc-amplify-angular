12cc1d285e03806df3670a7f233fe8267883b0ef
Juan Camilo Posso <juan.posso@cognits.co>
2025-03-29 21:30:32 -0500

    fix node vertion

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
-import outputs from './amplify_outputs.json';
 
 // Configuración CORRECTA para Amplify Gen 2:
 import { environment } from '../src/environments/environment';
