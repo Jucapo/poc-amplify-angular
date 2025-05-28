5503d572a5227dc8ccdbe1999fbddafdde9ec2be
Juan Camilo Posso <juan.posso@cognits.co>
2025-05-28 09:34:02 -0500

    change of logos, compodoc is added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,26 +1,4 @@
 // environment.development.ts
 export const environment = {
   production: false,
-  amplifyConfig: {
-    Auth: {
-      Cognito: {
-        userPoolId: 'us-east-1_MBb6bu3Y7',
-        userPoolClientId: '1rj7b7j2mifa8bbbr4pc1ptka5',
-        identityPoolId: 'us-east-1:2e61131b-1d6f-4714-9c8d-63a871e30a09',
-        loginWith: {
-          email: true,
-          phone: false,
-          username: false,
-        },
-      },
-    },
-    API: {
-      GraphQL: {
-        endpoint:
-          'https://xxxxxxxxxxxxxxx.appsync-api.us-east-1.amazonaws.com/graphql',
-        region: 'us-east-1',
-      },
-    },
-    region: 'us-east-1',
-  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,3 @@
 export const environment = {
   production: false,
-  amplifyConfig: {
-    Auth: {
-      Cognito: {
-        userPoolId: 'us-east-1_MBb6bu3Y7',
-        userPoolClientId: '1rj7b7j2mifa8bbbr4pc1ptka5',
-        identityPoolId: 'us-east-1:2e61131b-1d6f-4714-9c8d-63a871e30a09',
-        loginWith: {
-          email: true, // o username: true según tu configuración
-        },
-      },
-    },
-    region: 'us-east-1', // Esta propiedad va al nivel superior
-  },
 };
